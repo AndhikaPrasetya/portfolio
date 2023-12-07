327b550ec510c5ec4a06e20ed3060aafd597b7db
dhikaaaaaaaa <dhikaprasetya262@gmail.com>
2023-12-07 10:48:41 +0700

    updt

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,20 +14,22 @@ export const About = () => {
           className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
-        <li className={styles.aboutItem}>
+          <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
-              <h3>Bachelor Of Science Computer in Informatic Engineering, Mercu Buana University</h3>
+              <h3>
+                Bachelor Of Science Computer in Informatic Engineering, Mercu
+                Buana University
+              </h3>
               <p>
-              Major in Information System | Minor in Information Technology Management System | GPA: 3.75
+                Major in Information System | Minor in Information Technology
+                Management System | GPA: 3.75
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
               <h3>Himpunan Mahasiswa Sistem Informasi</h3>
-              <p>
-               Ketua Divisi Olahraga 2023-2024
-              </p>
+              <p>Head of Sports Division 2023-2024</p>
             </div>
           </li>
         </ul>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,10 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Andhika Prasetya </h1>
         <p className={styles.description}>
-        A third-year undergraduate student studying Information Systems and I have an interest in front-end development using Framework React JS. I also have a creative mind, quick learner, knowledge-curious, and adaptable person.
+          A third-year undergraduate student studying Information Systems and I
+          have the skills and experience in web development, especially as a
+          frontend developer and backend developer. I also have a quick learner,
+          knowledge-curious, creative mind and adaptable person.
         </p>
         <a href="#contact" className={styles.contactBtn}>
           Contact Me
